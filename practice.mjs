c5497316f351db08c6cf0ce1282b9e54eda9aa3a
Batsaikhan Ulambayar <Batsaikhan@Sugarmaas-MacBook-Pro.local>
2020-10-06 21:57:51 -0500

    Shuffle the Array

--- practice.mjs
+++ practice.mjs
@@ -27,3 +27,19 @@ var kidsWithCandies = function(candies, extraCandies) {
     }
     return result
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let result = []
+    let x = nums.slice(0, n)
+    let y = nums.slice(n)
+    for (let i=0; i<n; i++) {
+        result.push(x[i])
+        result.push(y[i])
+    }
+    return result
+};
