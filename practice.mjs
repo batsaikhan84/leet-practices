a4fc19715989c6474e600fe3af48d081c991c0d6
Batsaikhan Ulambayar <Batsaikhan@Sugarmaas-MacBook-Pro.local>
2020-10-12 21:14:48 -0500

    Shuffle String

--- practice.mjs
+++ practice.mjs
@@ -109,3 +109,16 @@ var maxDepth = function(s) {
     }
     return max
 };
+// Shuffle String
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let obj = {}
+    for(let i=0; i<indices.length; i++) {
+        obj[indices[i]] = s[i]
+    }
+    return Object.values(obj).join('')
+};
