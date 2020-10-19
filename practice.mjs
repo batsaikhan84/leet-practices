dbcc41d8176bf6741b79865dc8d53f04ac02d73c
Batsaikhan Ulambayar <Batsaikhan@Sugarmaas-MacBook-Pro.local>
2020-10-18 22:49:58 -0500

    Create Target Array in the Given Order

--- practice.mjs
+++ practice.mjs
@@ -168,3 +168,16 @@ var decompressRLElist = function(nums) {
     }
     return result
 };
+// Create Target Array in the Given Order
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function(nums, index) {
+    let result = []
+    for(let i=0; i<nums.length; i++) {
+        result.splice(index[i], 0, nums[i])
+    }
+    return result
+};
