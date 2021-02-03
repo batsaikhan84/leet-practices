90141becd419ab46cd3cbd8a3952613e1fdc4da8
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-02-02 22:45:36 -0600

    implement for loop

--- richest-customer-wealth.js
+++ richest-customer-wealth.js
@@ -1,3 +1,11 @@
 var maximumWealth = function(accounts) {
-    
+    arr = []
+    accounts.map(account => {
+        arr.push(account.reduce((a, b) => a + b, 0))
+    })
+    console.log(arr)
 };
+
+accounts = [[1,2,3],[3,2,1]]
+
+maximumWealth(accounts)
