bfdb45a31a6bcb44dfd0d9a897c61d77ccfd4e9c
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-02-02 22:10:27 -0600

    richest customer wealth initial commit

---
+++ jumping-on-the-clouds.js
@@ -0,0 +1,14 @@
+const findNumber = (arr) => {
+    newArr = arr.sort((a,b) => a - b)
+    for (let i = 0; i < newArr.length-1; i++) {
+        if (newArr[i] + 1 !== newArr[i+1]) {
+            console.log(newArr[i] + 1)
+        }
+    }
+}
+
+// arr = [3,7,1,2,8,4,5]
+arr = [2,3,4,6,7,8,9]
+
+findNumber(arr)
+
